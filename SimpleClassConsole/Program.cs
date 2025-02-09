0537e376acd9ea8744f0cd1715095e073e16a61c
Melanchenko_Andriy <ipz232_mad@student.ztu.edu.ua>
2025-02-09 18:23:42 +0200

    issues fixed

--- SimpleClassConsole/Program.cs
+++ SimpleClassConsole/Program.cs
@@ -1,74 +1,28 @@
-﻿using System.Text;
-using Microsoft.VisualBasic;
-using System;
+﻿using System;
+using System.Text;
 
 class Date
 {
-    protected int Year;
-    protected int Month;
-    protected int Day;
-    protected int Hours;
-    protected int Minutes;
+    public DateTime DateTimeValue { get; set; }
 
-    public Date()
-    {
-
-    }
     public Date(int year, int month, int day = 1, int hours = 0, int minutes = 0)
     {
-        Year = year;
-        Month = month;
-        Day = day;
-        Hours = hours;
-        Minutes = minutes;
+        DateTimeValue = new DateTime(year, month, day, hours, minutes, 0);
     }
 
-    public Date(Date copy)
+    public Date(DateTime dateTime)
     {
-        Year = copy.Year;
-        Month = copy.Month;
-        Day = copy.Day;
-        Hours = copy.Hours;
-        Minutes = copy.Minutes;
-
-    }
-    public int SetGetYear
-    {
-        get { return Year; }
-        set { Year = value; }
-    }
-    public int SetGetMonth
-    {
-        get { return Month; }
-        set { Month = value; }
-    }
-    public int SetGetDay
-    {
-        get { return Day; }
-        set { Day = value; }
-    }
-    public int SetGetHours
-    {
-        get { return Hours; }
-        set { Hours = value; }
-    }
-    public int SetGetMinutes
-    {
-        get { return Minutes; }
-        set { Minutes = value; }
+        DateTimeValue = dateTime;
     }
 }
+
 class Airplane
 {
-    protected string StartCity;
-    protected string FinishCity;
-    protected Date StartDate;
-    protected Date FinishDate;
+    public string StartCity { get; set; }
+    public string FinishCity { get; set; }
+    public Date StartDate { get; set; }
+    public Date FinishDate { get; set; }
 
-    public Airplane()
-    {
-
-    }
     public Airplane(string startCity, string finishCity, Date startDate, Date finishDate)
     {
         StartCity = startCity;
@@ -76,262 +30,61 @@ class Airplane
         StartDate = startDate;
         FinishDate = finishDate;
     }
-    public Airplane(string startCity, string finishCity, Date startDate)
+
+    public TimeSpan GetTotalTime()
     {
-        StartCity = startCity;
-        FinishCity = finishCity;
-        StartDate = startDate;
-    }
-    public Airplane(Airplane copy)
-    {
-        StartCity = copy.StartCity;
-        FinishCity = copy.FinishCity;
-        StartDate = copy.StartDate;
-        FinishDate = copy.FinishDate;
+        return FinishDate.DateTimeValue - StartDate.DateTimeValue;
     }
 
-    public string SetGetStartCity
-    {
-        get { return StartCity; }
-        set { StartCity = value; }
-    }
-    public string SetGetFinishCity
-    {
-        get { return FinishCity; }
-        set { FinishCity = value; }
-    }
-    public Date SetGetStartDate
-    {
-        get { return StartDate; }
-        set { StartDate = value; }
-    }
-    public Date SetGetFinishDate
-    {
-        get { return FinishDate; }
-        set { FinishDate = value; }
-    }
-    public int GetTotalTime()
-    {
-        int startTotalMinutes = StartDate.SetGetYear * 365 * 24 * 60 +
-                         StartDate.SetGetMonth * 30 * 24 * 60 +
-                         StartDate.SetGetDay * 24 * 60 +
-                         StartDate.SetGetHours * 60 +
-                         StartDate.SetGetMinutes;
-
-        int finishTotalMinutes = FinishDate.SetGetYear * 365 * 24 * 60 +
-                                 FinishDate.SetGetMonth * 30 * 24 * 60 +
-                                 FinishDate.SetGetDay * 24 * 60 +
-                                 FinishDate.SetGetHours * 60 +
-                                 FinishDate.SetGetMinutes;
-
-
-        return finishTotalMinutes - startTotalMinutes;
-    }
     public bool IsArriving()
     {
-        return StartDate.Equals(FinishDate);
+        return StartDate.DateTimeValue.Date == FinishDate.DateTimeValue.Date;
     }
-
-
 }
+
 class Program
 {
     static void Main()
     {
-
-Console.OutputEncoding = Encoding.Unicode;
+        Console.OutputEncoding = Encoding.Unicode;
         Console.InputEncoding = Encoding.Unicode;
-        Console.WriteLine("Виберіть номер завдання: ");
-        int num = int.Parse(Console.ReadLine());
-        switch (num)
-        {
-            case 1:
-                Airplane[] airplanes = ReadAirplaneArray();
-                Console.WriteLine("Сортування за датою:");
-                SortAirplanesByDate(airplanes);
-                PrintAirplanes(airplanes);
 
-                Console.WriteLine("\nСортування за загальним часом:");
-                SortAirplanesByTotalTime(airplanes);
-                PrintAirplanes(airplanes);
-
-                int minTravelTime, maxTravelTime;
-                GetAirplaneInfo(airplanes, out minTravelTime, out maxTravelTime);
-                Console.WriteLine($"\nМінімальний час подорожі: {minTravelTime} хвилин");
-                Console.WriteLine($"Максимальний час подорожі: {maxTravelTime} хвилин");
-
-                for (int i = 0; i < airplanes.Length; i++)
-                {
-                    Console.WriteLine($"Деталі польоту:");
-                    PrintAirplane(airplanes[i]);
-                    Console.WriteLine($"Загальний час подорожі: {airplanes[i].GetTotalTime()} хвилин");
-                    Console.WriteLine($"Прибуває: {airplanes[i].IsArriving()}");
-                }
-                break;
-            case 2:
-                AutoShow[] objects = AutoShow.Objects();
-                AutoShow.SortCars(objects);
-                Console.Title = "Лабораторна робота №5";
-                Console.SetWindowSize(100, 25);
-                Console.BackgroundColor = ConsoleColor.White;
-                Console.ForegroundColor = ConsoleColor.DarkBlue;
-                Console.ResetColor();
-                Console.BackgroundColor = ConsoleColor.White;
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write("Введіть ");
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.Write("число ");
-                Console.ForegroundColor = ConsoleColor.Blue;
-                Console.Write("масиву ");
-                Console.ForegroundColor = ConsoleColor.DarkRed;
-                Console.Write("структур ");
-                Console.ResetColor();
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine();
-                Console.WriteLine("Сортування за зростанням цін: ");
-                for (int i = 0; i < objects.Length; i++)
-                {
-                    Console.WriteLine($"Марка: {objects[i].GetCar}, Ціна: {objects[i].GetPrice}");
-                }
-                break;
-
-
-
-        }
-    }
-
-    static Airplane[] ReadAirplaneArray()
-    {
-        Console.WriteLine("Введіть кількість літаків: ");
+        Console.WriteLine("Введіть кількість літаків:");
         int n = int.Parse(Console.ReadLine());
 
         Airplane[] airplanes = new Airplane[n];
+
         for (int i = 0; i < n; i++)
         {
-            Console.WriteLine($"Введіть деталі для літака: {i + 1}");
+            Console.WriteLine($"Введіть деталі для літака {i + 1}:");
             Console.Write("Початкове місто: ");
             string startCity = Console.ReadLine();
             Console.Write("Кінцеве місто: ");
             string finishCity = Console.ReadLine();
-            Console.Write("Початкова дата (рік місяць день години хвилини): ");
-            string[] startDateInput = Console.ReadLine().Split(' ');
-            Date startDate = new Date(int.Parse(startDateInput[0]), int.Parse(startDateInput[1]), int.Parse(startDateInput[2]), int.Parse(startDateInput[3]), int.Parse(startDateInput[4]));
-            Console.Write("Кінцева дата (рік місяць день години хвилини): ");
-            string[] finishDateInput = Console.ReadLine().Split(' ');
-            Date finishDate = new Date(int.Parse(finishDateInput[0]), int.Parse(finishDateInput[1]), int.Parse(finishDateInput[2]), int.Parse(finishDateInput[3]), int.Parse(finishDateInput[4]));
-            airplanes[i] = new Airplane(startCity, finishCity, startDate, finishDate);
-            Console.WriteLine();
-        }
-        return airplanes;
 
-       
-}
-    static void PrintAirplane(Airplane airplane)
-    {
-        Console.WriteLine($"Початкове місто: {airplane.SetGetStartCity}");
-        Console.WriteLine($"Кінцеве місто: {airplane.SetGetFinishCity}");
-        Console.WriteLine($"Початкова дата: {airplane.SetGetStartDate.SetGetYear}/{airplane.SetGetStartDate.SetGetMonth}/{airplane.SetGetStartDate.SetGetDay} {airplane.SetGetStartDate.SetGetHours}:{airplane.SetGetStartDate.SetGetMinutes}");
-        Console.WriteLine($"Кінцева дата: {airplane.SetGetFinishDate.SetGetYear}/{airplane.SetGetFinishDate.SetGetMonth}/{airplane.SetGetFinishDate.SetGetDay} {airplane.SetGetFinishDate.SetGetHours}:{airplane.SetGetFinishDate.SetGetMinutes}");
+            Console.Write("Початкова дата (рік-місяць-день години:хвилини): ");
+            DateTime startDate = DateTime.Parse(Console.ReadLine());
+            Console.Write("Кінцева дата (рік-місяць-день години:хвилини): ");
+            DateTime finishDate = DateTime.Parse(Console.ReadLine());
+
+            airplanes[i] = new Airplane(startCity, finishCity, new Date(startDate), new Date(finishDate));
+        }
+
+        Console.WriteLine("Сортування за датою:");
+        Array.Sort(airplanes, (x, y) => x.StartDate.DateTimeValue.CompareTo(y.StartDate.DateTimeValue));
+        PrintAirplanes(airplanes);
     }
+
     static void PrintAirplanes(Airplane[] airplanes)
     {
-        for (int i = 0; i < airplanes.Length; i++)
+        foreach (var airplane in airplanes)
         {
-            Console.WriteLine($"Деталі польоту для літака номер {i + 1}:");
-            Console.WriteLine($"Початкове місто: {airplanes[i].SetGetStartCity}");
-            Console.WriteLine($"Кінцеве місто: {airplanes[i].SetGetFinishCity}");
-            if (airplanes[i].SetGetFinishDate.SetGetYear < airplanes[i].SetGetStartDate.SetGetYear)
-            {
-                Console.WriteLine("Початкова дата року не може бути більша за кінцеву");
-            }
-            Console.WriteLine($"Початкова дата: {airplanes[i].SetGetStartDate.SetGetYear}/{airplanes[i].SetGetStartDate.SetGetMonth}/{airplanes[i].SetGetStartDate.SetGetDay} {airplanes[i].SetGetStartDate.SetGetHours}:{airplanes[i].SetGetStartDate.SetGetMinutes}");
-            Console.WriteLine($"Кінцева дата: {airplanes[i].SetGetFinishDate.SetGetYear}/{airplanes[i].SetGetFinishDate.SetGetMonth}/{airplanes[i].SetGetFinishDate.SetGetDay} {airplanes[i].SetGetFinishDate.SetGetHours}:{airplanes[i].SetGetFinishDate.SetGetMinutes}");
+            Console.WriteLine($"Маршрут: {airplane.StartCity} → {airplane.FinishCity}");
+            Console.WriteLine($"Виліт: {airplane.StartDate.DateTimeValue}");
+            Console.WriteLine($"Приліт: {airplane.FinishDate.DateTimeValue}");
+            Console.WriteLine($"Час у польоті: {airplane.GetTotalTime()}");
+            Console.WriteLine($"Прибуває того ж дня: {airplane.IsArriving()}");
             Console.WriteLine();
         }
-    }
-    static void GetAirplaneInfo(Airplane[] airplanes, out int maxTravelTime, out int minTravelTime)
-    {
-        minTravelTime = airplanes[0].GetTotalTime();
-        maxTravelTime = airplanes[0].GetTotalTime();
-
-
-        for (int i = 1; i < airplanes.Length; i++)
-        {
-            int travelTime = airplanes[i].GetTotalTime();
-            if (travelTime < minTravelTime)
-            {
-                minTravelTime = travelTime;
-            }
-            else if (travelTime > maxTravelTime)
-            {
-                maxTravelTime = travelTime;
-            }
-        }
-    }
-    static void SortAirplanesByDate(Airplane[] airplanes)
-    {
-        Array.Sort(airplanes, (x, y) =>
-        {
-            int yearCompare = y.SetGetStartDate.SetGetYear.CompareTo(x.SetGetStartDate.SetGetYear);
-            if (yearCompare != 0)
-                return yearCompare;
-            int monthCompare = y.SetGetStartDate.SetGetMonth.CompareTo(x.SetGetStartDate.SetGetMonth);
-            if (monthCompare != 0)
-                return monthCompare;
-            int dayCompare = y.SetGetStartDate.SetGetDay.CompareTo(x.SetGetStartDate.SetGetDay);
-            return dayCompare;
-        });
-    }
-    static void SortAirplanesByTotalTime(Airplane[] airplanes)
-    {
-        Array.Sort(airplanes, (x, y) =>
-        {
-            int totalTime1 = x.GetTotalTime();
-            int totalTime2 = y.GetTotalTime();
-            return totalTime1.CompareTo(totalTime2);
-        });
-    }
-}
-
-class AutoShow
-{
-    protected string Car;
-    protected int Price;
-
-  
-public AutoShow(string car, int price)
-    {
-        Car = car;
-        Price = price;
-    }
-    public string GetCar
-    {
-        get { return Car; }
-    }
-    public int GetPrice
-    {
-        get { return Price; }
-    }
-    public static AutoShow[] Objects()
-    {
-        Console.WriteLine("Введіть кількість автосалонів: ");
-        int number = int.Parse(Console.ReadLine());
-        AutoShow[] objects = new AutoShow[number];
-        for (int i = 0; i < number; i++)
-        {
-            Console.WriteLine($"Введіть інформацію для автосалону номер {i + 1}");
-            Console.WriteLine("Марка автомобіля: ");
-            string car = Console.ReadLine();
-            Console.WriteLine("Ціна автомобіля: (в доларах) ");
-            int price = int.Parse(Console.ReadLine());
-
-            objects[i] = new AutoShow(car, price);
-        }
-        return objects;
-
-    }
-    public static void SortCars(AutoShow[] cars)
-    {
-        Array.Sort(cars, (x, y) => x.Price.CompareTo(y.Price));
     }
 }
